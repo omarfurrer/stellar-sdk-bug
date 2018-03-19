570d34427aafd69720845a689d0b96e0e1e9e826
Omar Furrer <omar.furrer@gmail.com>
2018-03-19 23:05:48 +0200

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use ZuluCrypto\StellarSdk\Keypair;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function testKeypair()
+    {
+        $sourceKeypair = Keypair::newFromSeed('SAA2U5UFW65DW3MLVX734BUQIHAWANQNBLTFT47X2NVVBCN7X6QC5AOG');
+        $this->assertEquals("GD4JRFLPF4AGYQTLCMZ7Q7DRLGQZQTGWOOKDUCNRVKG66G5ZVYYFT76M", $sourceKeypair);
+    }
+}
